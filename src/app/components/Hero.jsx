dda32a1641fef012f8207248e7a37e48bef17883
Nathan <nathk.nk1214@gmail.com>
2024-04-04 22:36:41 +0100

    Type Animation and Navigation
    Made the Hero section nicer to look at. Created the Navbar with the ability to add more links to the menu with ease and not needing to rely on copy-paste and cluttering the Nav component, keeping it neat!
    
    File(s) created:
    - /src/app/components/*Nav.jsx*
    - /src/app/components/*NavLink.jsx*
    - /src/app/components/*MenuOverlay.jsx*
    
    File(s) modified:
    - /src/app/*page.js*
    - /src/app/components/*Hero.jsx*
    - /src/app/components/*Nav.jsx*
    - /src/app/components/*NavLink.jsx*
    - /src/app/components/*MenuOverlay.jsx*
    
    > Created the animated text with React Type Animation "https://www.npmjs.com/package/react-type-animation".
    > Created the Mobile Navigation Bar/Overlay with the hamburger icon from Hero Icons "https://heroicons.com/"

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,28 +1,57 @@
-import React from "react";
-import Image from "next/image";
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
     <section>
-        <div className="grid grid-cols-1 lg:grid-cols-12">
-            <div className="col-span-7 place-self-center text-center sm:text-left">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl font-extrabold">
-                    Hi, I'm Nathan
+        <div className='grid grid-cols-1 lg:grid-cols-12'>
+            <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <h1 className='text-white mb-4 text-4xl sm:text-5xl font-extrabold'>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-red-600'>
+                        Hi, I'm {''}
+                    </span>
+                    <br></br>
+                    <TypeAnimation
+                        sequence={[
+                            'Nathan',
+                            1000,
+                            'Web Developer',
+                            1000,
+                            'IT Technician',
+                            1000
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        repeat={Infinity}
+                    />
                 </h1>
-                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 textl-lg lg:text-xl">
+                <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 textl-lg lg:text-xl'>
                     IT Technician, Web Developer and Soon to be Cybersecurity Technician.
                 </p>
                 <div>
-                <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Contact Me</button>
-                    <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">GitHub</button>
+                    
+                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-indigo-500 to-red-600 hover:bg-slate-200 text-white'>
+                        <span>
+                            Contact Me
+                        </span>
+                    </button>
+
+                    <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-indigo-500 to-red-600 hover:bg-slate-200 text-white mt-3'>
+                        <span className='block bg-[#121212] hover:bg-[#181818] rounded-full px-5 py-2'>
+                            See My GitHub
+                        </span>
+                    </button>
+
                 </div>
             </div>
-            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                <div className="bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative">
+            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                <div className='bg-[#181818] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative'>
                     <Image
-                    src="/images/hero.png"
-                    alt="[hero.png] - Hero Image"
-                    className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                    src='/images/hero.png'
+                    alt='[hero.png] - Hero Image'
+                    className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
                     width={300}
                     height={300}
                     />
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import Image from "next/image";
+<script src="http://localhost:8097"></script>
+import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Nav/>
       <div className="container mx-auto px-12 py-4">
         <Hero/>
       </div>
