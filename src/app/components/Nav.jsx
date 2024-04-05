56cca49be9e1b5464952ae88540d13e302897809
Kitchvx <nathan@nkitch.com>
2024-04-05 21:01:24 +0100

    Clean up
    Cleaned up the speech marks and replaced them with apostrophes to unify the code (I often switch between them like a maniac). Corrected the version in the package.json.
    
    File(s) modified:
    - /src/app/*package.json*
    - /src/app/components/*Nav.jsx*

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -7,16 +7,16 @@ import MenuOverlay from "./MenuOverlay";
 
 const NavLinks = [
   {
-    title: "About",
-    path: "#about",
+    title: 'About',
+    path: '#about',
   },
   {
-    title: "Projects",
-    path: "#projects",
+    title: 'Projects',
+    path: '#projects',
   },
   {
-    title: "Contact",
-    path: "#contact",
+    title: 'Contact',
+    path: '#contact',
   },
 ];
 
@@ -30,25 +30,25 @@ const Nav = () => {
           href={'/'} className='text-2xl md:text-3xl text-[#CACACA] m:text-xl rounded md:p-0 hover:text-white font-bold'>
           Nathan Kitching
         </Link>
-        <div className="mobile-menu block md:hidden">
+        <div className='mobile-menu block md:hidden'>
           {!NavOpen ? (
             <button
               onClick={() => setNavOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
             >
-              <Bars3Icon className="h-5 w-5" />
+              <Bars3Icon className='h-5 w-5' />
             </button>
           ) : (
             <button
               onClick={() => setNavOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'
             >
-              <XMarkIcon className="h-5 w-5" />
+              <XMarkIcon className='h-5 w-5' />
             </button>
           )}
         </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+        <div className='menu hidden md:block md:w-auto' id='navbar'>
+          <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
             {NavLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title} />
